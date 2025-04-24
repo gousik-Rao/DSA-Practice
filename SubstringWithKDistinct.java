aa296f61a928048a079b419ca4776d68c6db8584
Gousik <gousikthason@gmail.com>
2025-04-24 23:32:12 +0530

    Modified Substring with K distinct elements

--- SubstringWithKDistinct.java
+++ SubstringWithKDistinct.java
@@ -1,8 +1,45 @@
 package pack.DSA.Week3;
 
+import java.util.HashMap;
+
 public class SubstringWithKDistinct {
 
-	int countSubstring(String s, int k) {
+	public static void main(String[] args) {
+		System.out.println(countSubstr("abaaca", 1));
+	}
+	
+	static int countSubstr(String s, int k) {
+        // your code here
+        
+        int count = countSubstringsWithAtMostK(k, s) - countSubstringsWithAtMostK(k - 1, s);             
+        
+        return count;
+    }
+	
+	static int countSubstringsWithAtMostK(int k, String s) {
 		
+        int n = s.length();
+        int result = 0;
+        char[] ch = s.toCharArray();
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for(int left = 0, right = 0; right < n; right++) {
+        	map.put(ch[right], map.getOrDefault(ch[right], 0) + 1);
+        	
+        	while(map.size() > k) {
+        		map.put(ch[left], map.get(ch[left]) - 1);
+        		if(map.get(ch[left]) == 0) {
+            		map.remove(ch[left]);
+            	}
+        		left++;
+        	}
+        	
+        	result += (right - left) + 1;
+        	
+        }
+        
+        
+        return result;
 	}
 }
