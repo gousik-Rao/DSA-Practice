fd52c130208fa983fd631164c47038282f4896f9
Gousik <gousikthason@gmail.com>
2025-05-02 07:43:17 +0530

    optimized the parntheses generation logic to use StringBuilder instead of String to avoid memory over head

--- GenerateParenthesis.java
+++ GenerateParenthesis.java
@@ -1,5 +1,33 @@
 package pack.DSA.Week4;
 
-public class GenerateParenthesis {
+import java.util.ArrayList;
+import java.util.List;
 
+public class GenerateParenthesis {
+	
+	public static void main(String... args) {
+		System.out.println(generateParenthesis(3));
+	}
+	
+	static public List<String> generateParenthesis(int n){
+		return backtrack(new StringBuilder(), 0, 0, new ArrayList<String> (), n);
+	}
+	
+	static List<String> backtrack(StringBuilder currentString, int openCount, int closeCount, List<String> res, int n){                   
+		if(openCount == n && closeCount == n) {
+			res.add(currentString.toString());
+			return res;
+		}
+		
+		if(openCount < n) {
+			backtrack(currentString.append("("), openCount + 1, closeCount, res, n);
+			currentString.deleteCharAt(currentString.length() - 1);
+		}
+		if(closeCount < openCount) {
+			backtrack(currentString.append(")"), openCount, closeCount + 1, res, n);
+			currentString.deleteCharAt(currentString.length() - 1);
+		}
+		
+		return res;
+	}
 }
