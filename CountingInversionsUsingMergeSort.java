c2c831e327024ff630259236a1b9f0191833695c
Gousik <gousikthason@gmail.com>
2025-06-19 21:23:30 +0530

    Count the no .of inversions in a array using mergse sort

--- CountingInversionsUsingMergeSort.java
+++ CountingInversionsUsingMergeSort.java
@@ -2,53 +2,69 @@ package pack.DSA.Week2;
 
 public class CountingInversionsUsingMergeSort {
 
+	public static void main(String[] args) {
+		System.out.println(inversionCount(new int[] {2, 4, 1, 3, 5}));
+	}
+	
 	// Function to count inversions in the array.
     static int inversionCount(int arr[]) {
         // Your Code Here
         return countInversions(arr, 0, arr.length);
     }
-    
-    static private int countInversions(int[] arr, int start, int end){
-        if(end - start <= 1){
-            return 0;
-        }
-        
-        int mid = start + ((end - start) >> 1);
 
-        int left = countInversions(arr, start, mid);
-        int right = countInversions(arr, mid, end);
-        
-        int result = merge(arr, start, mid, end);
-        
-        return result + right + left;
-    }
-    
-    static private int merge(int arr[], int s, int m, int e){
-        int i = s, j = m, k = 0;
-        int[] result = new int[e - s];
-        int count = 0;
+	private static int countInversions(int[] arr, int start, int end) {                    
+		if(end - start <= 1) return 0;
+		
+		int mid = start + ((end - start) >> 1);
+		
+		int left = countInversions(arr,start, mid);
+		int right = countInversions(arr, mid, end);
+		
+		int result = crossCounts(arr, start, mid, end);
+		
+		return result + left + right;
+	}
 
-        while(i < m && j < e){
-            if(arr[i] > arr[j]){
-        /* -> */count += (m - i); // Most important for achieving O(n Log n)               
-                result[k] = arr[j++];
-            }else{
-            	result[k] = arr[i++];
-            }
-            k++;
-        }
-        
-        // Here we are swapping the remain elements in ascending order
-        while(i < m) result[k++] = arr[i++];
-        
-        // This is for second array
-        while(j < e) result[k] = arr[j];
-        
-        // Deep copy of array
-        for(int idx = 0; idx < result.length; idx++){
-            arr[idx + s] = result[idx];
-        }
-        
-        return count;
-    }
+	// In - Place merging
+	private static int crossCounts(int[] arr, int start, int mid, int end) {
+		int idx = start, jdx = mid, kdx = 0;
+		int[] res = new int[end - start];
+		int count = 0;
+		while(idx < mid && jdx < end) {
+			if(arr[idx] > arr[jdx]) {
+				res[kdx++] = arr[jdx++];
+				count += mid - idx; // Important line as the elements from idx to mid will be inversed 
+									// if the condition is true As the array is sorted!!
+			}else {
+				res[kdx++] = arr[idx++];
+			}
+		}
+		
+		while(idx < mid) {
+			res[kdx++] = arr[idx++];
+		}
+		
+		while(jdx < end) {
+			res[kdx++] = arr[jdx++];
+		}
+		
+		// Deep copy of array
+		for(int index = 0; index < res.length; index++) {
+			arr[index + start] = res[index];
+		}
+		
+		return count;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
